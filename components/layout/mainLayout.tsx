524f56e2da5dab271b487e0ddb90c06982d232a4
Alex Hua <huazifan@gmail.com>
2022-10-19 15:49:53 +0800

    move welcome message to header

--- components/layout/mainLayout.tsx
+++ components/layout/mainLayout.tsx
@@ -1,3 +1,4 @@
+import Header from "./header";
 import Sidebar from "./sidebar";
 
 export default function MainLayout({ children }: any) {
@@ -7,7 +8,12 @@ export default function MainLayout({ children }: any) {
         <Sidebar />
         <div className="flex flex-col min-h-screen items-stretch ml-16 w-full">
           <div className="flex-grow  text-black dark:text-zinc-50 w-full">
-            <main className="flex-shrink-0 flex items-center justify-center">{children}</main>
+            <main className="flex-shrink-0 flex items-center justify-center">
+              <div className="w-full mx-4">
+                <Header />
+                {children}
+              </div>
+            </main>
           </div>
           <div className="flex-shrink-0">
           </div>
--- pages/category.tsx
+++ pages/category.tsx
@@ -5,26 +5,26 @@ import CategoryWithSubcategories from "../components/navCategory/categoryWithSub
 
 export default function Category(): JSX.Element {
       return (
-            <div className="w-full mx-4">
-                  <Welcome>Welcome to Alex222222222222 Blog</Welcome>
-
-                  {nav.categories.map(({ name, description, fatherCategory, subCategory, links,icon, hidden }) => {
-                        if (fatherCategory == "") {
-                              if (subCategory.length > 0) {
-                                    return (
-                                          <CategoryWithSubcategories name={name} description={description} icon={icon} hidden={hidden} links={links} subCategory={subCategory} />
-                                    )
+            <>
+                  {
+                        nav.categories.map(({ name, description, fatherCategory, subCategory, links, icon, hidden }) => {
+                              if (fatherCategory == "") {
+                                    if (subCategory.length > 0) {
+                                          return (
+                                                <CategoryWithSubcategories name={name} description={description} icon={icon} hidden={hidden} links={links} subCategory={subCategory} />
+                                          )
+                                    } else {
+                                          return (
+                                                <SingleCategory name={name} description={description} icon={icon} hidden={hidden} links={links} />
+                                          )
+                                    }
                               } else {
-                                    return (
-                                          <SingleCategory name={name} description={description} icon={icon} hidden={hidden} links={links} />
-                                    )
+                                    return ""
                               }
-                        } else {
-                              return ""
-                        }
 
-                  })}
-            </div>
+                        })
+                  }
+            </>
       )
 }
 
