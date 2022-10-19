afb9166192af5cdb315ff00ee823681aeebfbf70
Alex Hua <huazifan@gmail.com>
2022-10-19 09:29:46 +0800

    init commit
    a website with navigation page and blogs using next.js with typescript
    and tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  prefix: '',
+  important: true,
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}', 
+    './components/**/*.{js,ts,jsx,tsx}',
+    './store/*.{js,ts,jsx,tsx}',
+    './pages/*.{js,ts,jsx,tsx}'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+};
