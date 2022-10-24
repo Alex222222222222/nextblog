847b31d4569dafd8140b040a0cd902b719b14b90
Alex Hua <huazifan@gmail.com>
2022-10-24 13:17:22 +0800

    change to new method to get link data
    first create a const linksIndexedById and directly get links from id to
    decrease delay

--- lib/link.tsx
+++ lib/link.tsx
@@ -1,12 +1,20 @@
 import { URLLink, blankLink } from "../interface/link";
 import nav from "../nav.json"
 
-export function getLinkFromID(name:String):URLLink{
-      for (let l of nav.links) {
-            if (l.id.toString() == name) {
-                  return l
+function linksIndexedById(): Map<number, URLLink> {
+      const links: Map<number, URLLink> = new Map()
+      nav.links.forEach(
+            (link) => {
+                  links.set(link.id, link)
             }
-      }
-      
-      return blankLink
+      )
+
+      return links
+}
+
+export const allLinksIndexedById: Map<number, URLLink> = linksIndexedById()
+
+export function getLinkFromID(name:String):URLLink{
+      const id = parseInt(name as string)
+      return allLinksIndexedById.get(id) ?? blankLink
 }
